2f7d27c5cf53d292f250fadace63fe1f1198de61
Suleiman <80757260+Sa1tama54@users.noreply.github.com>
2022-06-22 10:29:52 +0300

    написал скелет для сервера

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const logger = require("morgan");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger(':method :status :url "HTTP/http-version"'));
 app.use(require("./routes"));
 
 app.use("/public", express.static(path.resolve(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.end("Morgan Logger App");
+});
 
 mongoose.connect(process.env.MONGODV_SERVER).then(() => {
   app.listen(process.env.PORT, () =>
