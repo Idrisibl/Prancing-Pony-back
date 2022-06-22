0848741e54c07bc5150d11199ab3b2c165da9b30
Idris <iblaev.idris@mail.ru>
2022-06-22 16:23:43 +0300

    Логика заданий прописана

---
+++ controllers/tasks.controller.js
@@ -0,0 +1,74 @@
+const Task = require("../models/Task.model");
+
+module.exports.taskController = {
+  createTask: async (req, res) => {
+    const { categories, title, text, price, completed, left } = req.body;
+
+    try {
+      const task = await Task.create({
+        categories,
+        title,
+        text,
+        price,
+        completed,
+        left,
+        user: req.user.id
+      });
+      return res.json(task);
+    } catch (error) {
+      return res.json({ error: "Ошибка создания задания" });
+    }
+  },
+
+  getAllTasks: async (req, res) => {
+    try {
+      const task = await Task.find({}).populate("categories user");
+      res.json(task);
+    } catch (error) {
+      res.json({ error: "Ошибка при вызове всех заданий" });
+    }
+  },
+
+  getTaskById: async (req, res) => {
+    try {
+      const task = await Task.findById(req.params.id).populate(
+        "categories user"
+      );
+      res.json(task);
+    } catch (error) {
+      res.json({ error: "Ошибка при вызове задания по id" });
+    }
+  },
+  getTasksOnCategories: async (req, res) => {
+    try {
+      const task = await Task.find({
+        categories: req.params.id,
+      }).populate("categories user");
+      res.json(task);
+    } catch (error) {
+      res.json({ error: "Ошибка при вызове заданий определенной категории" });
+    }
+  },
+  delTask: async (req, res) => {
+    try {
+     const task = await Task.findByIdAndRemove(req.params.id);
+      res.json(task);
+    } catch (error) {
+      res.json({error: "Ошибка при удалении задания"});
+    }
+  },
+
+  //
+  patchTask: async (req, res) => {
+    try {
+     const task = await Task.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        text: req.body.text,
+        price: req.body.price
+      });
+      res.json(task);
+    } catch (error) {
+      res.json({error : "Ошибка при изменении задания"});
+    }
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const { Router } = require("express");
 
 const router = Router();
 
+router.use(require("./tasks.route"));
+
 module.exports = router;
