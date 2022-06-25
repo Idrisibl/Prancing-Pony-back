e3d45a4d5d902dca087f76d1cbb7cfa934112a57
Suleiman <80757260+Sa1tama54@users.noreply.github.com>
2022-06-25 14:42:10 +0300

    исправил роуты и контроллеры юзера

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,50 +1,28 @@
 const { Router } = require("express");
 const { userController } = require("../controllers/user.controller");
-const { check } = require("express-validator");
 const fileMiddleware = require("../middlewares/file.middleware");
 
 const router = Router();
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
-router.post(
-  "/register",
-  [
-    check("name", "Это поле не должно быть пустым").notEmpty(),
-    check("lastname", "Это поле не должно быть пустым").notEmpty(),
-    check("email", "Это поле не должно быть пустым").notEmpty(),
-    check("email", "Некорректный email").isEmail(),
-    check("password", "Это поле не должно быть пустым").notEmpty(),
-    check(
-      "password",
-      "Пароль должен быть больше 4 и меньше 12 символов"
-    ).isLength({
-      min: 5,
-      max: 12,
-    }),
-  ],
-  userController.registerUser
+router.post("/register", userController.registerUser);
+router.post("/login", userController.login);
+router.patch(
+  "/avatar",
+  fileMiddleware.single("avatar"),
+  userController.editAvatar
 );
-router.post(
-  "/login",
-  [
-    check("email", "Это поле не должно быть пустым").notEmpty(),
-    check("email", "Некорректный email").isEmail(),
-    check("password", "Это поле не должно быть пустым").notEmpty(),
-  ],
-  userController.login
-);
-router.patch("/avatar/:id", fileMiddleware.single(), userController.editAvatar);
-router.patch("/editUser/:id", userController.editUser);
-router.patch("/bag/:id", userController.fillTheBag);
-router.patch("/bag/remove/:id", userController.removeFromBag);
-router.patch("/favoutire/:id", userController.addToFavourite);
-router.patch("/favoutire/remove/:id", userController.removeFromFavourite);
-router.patch("/finished/:id", userController.addToFinished);
-router.patch("/friends/:id", userController.addToFriends);
-router.patch("/friends/remove/:id", userController.removeFromFriends);
-router.patch("/blacklist/:id", userController.addToBlacklist);
-router.patch("/blacklist/remove/:id", userController.removeFromBlacklist);
+router.patch("/editUser", userController.editUser);
+router.patch("/bag", userController.fillTheBag);
+router.patch("/bag/remove", userController.removeFromBag);
+router.patch("/favoutire", userController.addToFavourite);
+router.patch("/favoutire/remove", userController.removeFromFavourite);
+router.patch("/finished", userController.addToFinished);
+router.patch("/friends", userController.addToFriends);
+router.patch("/friends/remove", userController.removeFromFriends);
+router.patch("/blacklist", userController.addToBlacklist);
+router.patch("/blacklist/remove", userController.removeFromBlacklist);
 router.patch("/rating/:id", userController.addToRating);
 router.patch("/responces/:id", userController.addToResponces);
 router.patch("/responces/remove/:id", userController.removeFromResponces);
